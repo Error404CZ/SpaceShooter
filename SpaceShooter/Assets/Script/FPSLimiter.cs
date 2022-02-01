36a7c0c363acca76a8e0a5132260a18fa6afe297
Filip Hobza <filip.hobza.2005@gmail.com>
2022-02-01 08:37:09 +0100

    FPSLimiter, Asteroids live
    - opravit  rychlost při málo FPS

---
+++ SpaceShooter/Assets/Script/FPSLimiter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSLimiter : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int targetFrameRate = 30;
+ 
+    private void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFrameRate;
+    }
+}
